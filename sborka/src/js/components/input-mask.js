2a41a5811f10077cdde74b376f5994edbec005fb
Mikhail Yanov <yanovmikhailco@ya.ru>
2022-08-03 13:56:56 +0200

    sborka

---
+++ sborka/src/js/components/input-mask.js
@@ -0,0 +1,4 @@
+import Inputmask from 'inputmask'
+const selector = document.querySelector('#tel')
+const im = new Inputmask('+7 (999) 999-99-99')
+im.mask(selector)
---
+++ sborka/src/js/index.js
@@ -0,0 +1,10 @@
+import { isWebp } from './components/is-webp.js'
+// import { hello } from './components/greet.ts'
+// import './components/page-slider.js'
+// import './components/hero-slider.ts'
+// import './components/result-slider.ts'
+// import './components/menu.js'
+// import './components/validate-contacts.js'
+
+isWebp()
+// hello('TypeScript')
